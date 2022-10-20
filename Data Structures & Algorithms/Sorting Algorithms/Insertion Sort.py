7a1e060eaa930148be11ddf06193caf142069350
sezanhaque <sezansarker@gmail.com>
2022-10-20 21:35:17 +0600

    Insertion Sort

---
+++ Data Structures & Algorithms/Sorting Algorithms/Insertion Sort.py
@@ -0,0 +1,29 @@
+def insertionSort(arr: list[int]) -> list[int]:
+    for step in range(1, len(arr)):
+        key = arr[step]
+        j = step - 1
+
+        # Compare key with each element on the left of it until an element smaller than it is found
+        # For descending order, change key<arr[j] to key>arr[j].
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j = j - 1
+            count += 1
+
+        # Place key at after the element just smaller than it.
+        arr[j + 1] = key
+
+
+def insertionSort(arr: list[int]) -> list[int]:
+    for i in range(len(arr) - 1):
+        for j in range(i + 1, 0, -1):
+            if arr[j] < arr[j - 1]:
+                arr[j], arr[j - 1] = arr[j - 1], arr[j]
+                count += 1
+            else:
+                break
+
+
+data = [9, 5, 1, 4, 3]
+insertionSort(data)
+print(data)
---
+++ Problems/147. Insertion Sort List.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def insertionSortList(self, head):
+        dummy = ListNode(-1)
+        curr = head
+
+        while curr:
+            curr_next = curr.next
+            prv, nxt = dummy, dummy.next
+
+            while nxt:
+                if nxt.val > curr.val:
+                    break
+                prv = nxt
+                nxt = nxt.next
+
+            curr.next = nxt
+            prv.next = curr
+            curr = curr_next
+
+        return dummy.next
