566b4e1157186da48b7aa15c4ca8098538e8da63
sezanhaque <sezansarker@gmail.com>
2023-04-10 22:30:09 +0600

    20 updated

--- Problems/Python/0020. Valid Parentheses.py
+++ Problems/Python/0020. Valid Parentheses.py
@@ -47,3 +47,24 @@ def isValidTwo(self, s: str) -> bool:
 
 
 print(isValidTwo(0, "({})[()]{}"))
+
+
+class Solution:
+    def isValidTwo(self, s: str) -> bool:
+        stack = []
+        close_to_open = {")": "(", "}": "{", "]": "["}
+
+        for char in s:
+            if char in close_to_open:
+                if stack and stack[-1] == close_to_open[char]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(char)
+
+        return True if not stack else False
+
+
+obj = Solution()
+print(obj.isValidTwo("({})[()]{}"))
