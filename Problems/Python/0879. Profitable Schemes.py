9a73ca87f96df7786d8fb7954e885bde08e84ae8
sezanhaque <sezansarker@gmail.com>
2023-04-21 15:03:34 +0600

    879

---
+++ Problems/Python/0879. Profitable Schemes.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def profitableSchemes(self, n: int, minProfit: int, group: List[int], profit: List[int]) -> int:
+        dp = [[0] * (n + 1) for _ in range(minProfit + 1)]
+        dp[0][0] = 1
+
+        for p, g in zip(profit, group):
+            for i in range(minProfit, -1, -1):
+                for j in range(n - g, -1, -1):
+                    dp[min(i + p, minProfit)][j + g] += dp[i][j]
+
+        return sum(dp[minProfit]) % (10 ** 9 + 7)
+
+
+obj = Solution()
+print(obj.profitableSchemes(n=5, minProfit=3, group=[2, 2], profit=[2, 3]))
