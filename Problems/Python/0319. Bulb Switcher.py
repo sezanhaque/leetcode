5d91cee3ada6390b9d4df2bcd413247f0793ec60
sezanhaque <sezansarker@gmail.com>
2023-04-27 23:55:39 +0600

    319

---
+++ Problems/Python/0319. Bulb Switcher.py
@@ -0,0 +1,19 @@
+from math import sqrt
+
+
+class Solution:
+    """
+    A bulb ends up on iff it is switched an odd number of times.
+
+    Call them bulb 1 to bulb n. Bulb i is switched in round d if and only if d divides i.
+    So bulb i ends up on if and only if it has an odd number of divisors.
+
+    Divisors come in pairs, like i=12 has divisors 1 and 12, 2 and 6, and 3 and 4.
+    Except when i is a square, like 36 has divisors 1 and 36, 2 and 18, 3 and 12, 4 and 9, and double divisor 6.
+    So bulb i ends up on if and only if i is a square.
+    """
+
+    def bulbSwitch(self, n: int) -> int:
+        # return int(n ** 0.5)
+        # or
+        return int(sqrt(n))
